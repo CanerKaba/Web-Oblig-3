25f30a71d738d067a7925808f99060bd00d00aca
cakab8571 <cakab8571@oslomet.no>
2024-04-25 19:00:35 +0200

    Initial commit

---
+++ src/main/java/com/example/weboblig3/WebOblig3Application.java
@@ -0,0 +1,13 @@
+package com.example.weboblig3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOblig3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOblig3Application.class, args);
+	}
+
+}
